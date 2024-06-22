9bfb1c5c6633b21974ad68459a7616ea158dd752
yaghoubi-mn <yaghoubi.mn@gmail.com>
2024-06-22 23:34:01 +0330

    fix cors errors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,5 +139,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000'
+    'http://localhost:8000',
+    'https://localhost:8000'
 )
