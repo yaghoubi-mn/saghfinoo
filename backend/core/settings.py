91e73bde2c5cb0ef2e5511ee840a1644fd801a75
yaghoubi-mn <yaghoubi.mn@gmail.com>
2024-06-22 23:23:49 +0330

    rename real_estate_agent and cors to settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,6 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'news',
+    'real_estate_agents',
+    'real_estate_offices',
+    'real_estates',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +137,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
