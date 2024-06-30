df21c322ba0d3f2626848ebf952e8372442f1ff9
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-06-30 21:05:44 +0330

    fix error

--- backend/common/utils/error_handlers.py
+++ backend/common/utils/error_handlers.py
@@ -3,11 +3,11 @@ from rest_framework.views import exception_handler
 def custom_error_handler(exc, context):
     # call rest framework default error handler first
     response = exception_handler(exc, context)
-    
-    response.data = customize_error_messages(response.data)
+    if response:
+        response.data = customize_error_messages(response.data)
 
-    response.data['status'] = response.status_code
-    response.status_code = 200
+        response.data['status'] = response.status_code
+        response.status_code = 200
 
     return response
 
