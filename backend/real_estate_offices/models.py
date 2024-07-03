e28877df1f3aa217051ef0251079933f686b0d4a
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-04 00:05:28 +0330

    add model

--- backend/real_estate_offices/models.py
+++ backend/real_estate_offices/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class RealEstateOffice(models.Model):
+    # owner = models.ForeignKey(RealEstateAgent)
+    name = models.CharField(max_length=50)
+    score = models.FloatField()
+    description = models.CharField(max_length=1000)
+    
+    city = models.CharField(max_length=50)
+    main_street = models.CharField(max_length=50)
+    sub_street = models.CharField(max_length=50)
+    
+    number_of_active_ads = models.PositiveIntegerField()
+    number_of_comments = models.PositiveIntegerField()
+
+    number = models.CharField(max_length=11)
+    landline_number = models.CharField(max_length=50)
+
