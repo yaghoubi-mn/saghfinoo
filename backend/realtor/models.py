722c51a98f597df70c9e9a4cc857e60577b6af07
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-05 18:02:51 +0330

    develop realtor

--- backend/realtor/models.py
+++ backend/realtor/models.py
@@ -5,12 +5,12 @@ from real_estate_offices.models import RealEstateOffice
 
 class Realtor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    real_estate_office = models.ForeignKey(RealEstateOffice, on_delete=models.SET_DEFAULT, null=True)
+    real_estate_office = models.ForeignKey(RealEstateOffice, on_delete=models.SET_DEFAULT, null=True, default=None)
+    is_confirmed_by_real_estate_office = models.BooleanField(default=False) # must confirmed by real estate office owner
 
-    score = models.FloatField()
+    score = models.FloatField(default=5)
+    number_of_active_ads = models.PositiveIntegerField(default=0)
+
     description = models.CharField(max_length=200)
-
-    number_of_active_ads = models.PositiveIntegerField()
-
     number = models.CharField(max_length=11)
     landline_number = models.CharField(max_length=50)
--- backend/realtor/views.py
+++ backend/realtor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .serializers import RealtorSerializer
+
+class CreateRealtor(APIView):
+    serializer_class = RealtorSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, req):
+        serializer = self.serializer_class(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"done", 'status':200})
+        return Response({"errors":serializer.error_messages, 'status':400})
