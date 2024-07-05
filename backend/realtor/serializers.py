03e09c420c9f60c97c945b3bc4c2c0892711d6ff
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-05 18:06:36 +0330

    remove realtor

--- backend/realtor/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Realtor
-
-class RealtorSerializer(serializers.ModelField):
-
-    class Meta:
-        model = Realtor
-        fields = ['description', 'number', 'landline_number']
--- backend/realtor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('create', views.CreateRealtor.as_view(), name='create_realtor'),
-]
