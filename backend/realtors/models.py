6f168ff84aa78acccef8aa259e4403e78f48ad71
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-06 23:46:21 +0330

    fix model and makemigrations

--- backend/realtors/models.py
+++ backend/realtors/models.py
@@ -7,6 +7,7 @@ class Realtor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     real_estate_office = models.ForeignKey(RealEstateOffice, on_delete=models.SET_DEFAULT, null=True, blank=True, default=None)
     is_confirmed_by_real_estate_office = models.BooleanField(default=False) # must confirmed by real estate office owner
+    is_confirmed = models.BooleanField(default=False) # must confirmed by admin
 
     score = models.FloatField(default=5)
     number_of_active_ads = models.PositiveIntegerField(default=0)
@@ -14,3 +15,9 @@ class Realtor(models.Model):
     description = models.CharField(max_length=200)
     number = models.CharField(max_length=11)
     landline_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        reo = ''
+        if self.real_estate_office:
+            reo = self.real_estate_office.name
+        return self.user.first_name +' '+ self.user.last_name +', '+ reo
