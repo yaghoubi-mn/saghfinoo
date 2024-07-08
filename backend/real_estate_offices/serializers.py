18c43386d0a6f11614f7c555bbec05953ad0bfa4
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-08 16:07:47 +0330

    add validations to seralisers

--- backend/real_estate_offices/serializers.py
+++ backend/real_estate_offices/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from common.utils import validations
 from .models import RealEstateOffice
 
 class RealEstateOfficeSerializer(serializers.ModelSerializer):
@@ -20,6 +21,23 @@ class RealEstateOfficeSerializer(serializers.ModelSerializer):
             'instagram',
             'telegram',
         ]
+
+    def validate(self, attrs):
+
+        validations.validate_se('name', attrs['name'], validations.validate_name)
+        validations.validate_se('description', attrs['description'], validations.validate_description)
+        validations.validate_se('username', attrs['username'], validations.validate_username)
+        validations.validate_se('city', attrs['city'], validations.validate_name)
+        validations.validate_se('main_street', attrs['main_street'], validations.validate_name)
+        validations.validate_se('sub_street', attrs['sub_street'], validations.validate_name)
+        validations.validate_se('number', attrs['number'], validations.validate_number)
+        validations.validate_se('landline_number', attrs['landline_number'], validations.validate_landline_number)
+        validations.validate_se('site', attrs['site'], validations.validate_username)
+        validations.validate_se('linkedin', attrs['linkedin'], validations.validate_username)
+        validations.validate_se('telegram', attrs['telegram'], validations.validate_username)
+        validations.validate_se('instagram', attrs['instagram'], validations.validate_username)
+
+        return super().validate(attrs)
 
 class RealEstateOfficeResponseSerializer(serializers.ModelSerializer):
 
