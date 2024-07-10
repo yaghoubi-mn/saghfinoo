132258b62c96f1e784632dae328bf63b1b892426
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-10 17:03:34 +0330

    move codes into one file

--- backend/common/utils/permissions.py
+++ backend/common/utils/permissions.py
@@ -1,14 +1,23 @@
 from rest_framework.permissions import BasePermission
 
 from . import realtor_model
+from real_estate_offices.models import RealEstateOffice
 
 class IsOwner(BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner
+    def has_permission(self, request, view):
+        try:
+            request.real_estate_office = RealEstateOffice.objects.get(user=request.user)
+            return True
+        except RealEstateOffice.DoesNotExist:
+            return False
+            
     
 class IsAdmin(BasePermission):
-    
+
+    def has_permission(self, request, view):
+        return False
+
     def has_object_permission(self, request, view, obj):
         return True
         # todo: complete
