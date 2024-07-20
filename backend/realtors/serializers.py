10f6d36d0ee55c0a6ddd5ffb51c884b7a161c731
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-20 13:23:10 +0330

    add view and serializer for realtor

--- backend/realtors/serializers.py
+++ backend/realtors/serializers.py
@@ -16,3 +16,20 @@ class RealtorSerializer(serializers.ModelSerializer):
         validations.validate_se('landline_number', attrs['landline_number'], validations.validate_landline_number)
 
         return super().validate(attrs)
+
+class RealtorResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Realtor
+        fields = ['user__first_name', 'user__last_name', 'user__image_full_path', 'bg_imgae_full_path', 'score', 'number_of_active_ads', 'description', 'number', 'landline_number', 'real_estate_office__name', 'real_estate_office__username']
+
+
+
+class RealtorPreviewResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Realtor
+        fields = ['id', 'user__first_name', 'user__last_name', 'user__image_full_path', 'score', 'real_estate_office__name', 'real_estate_office__username']
+
+
+
--- backend/realtors/urls.py
+++ backend/realtors/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.CreateRealtor.as_view(), name='create_realtor'),
+    path('get-all', views.GetAllRealtorAPIView.as_view(), name='get_all_realtor'),
+    path('get/<realtor_id>', views.GetRealtorAPIView.as_view(), name='get_realtor'),
 ]
