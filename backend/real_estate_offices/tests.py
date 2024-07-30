dbfc39f7c188c6dc7f0db20aaa975abb87503b06
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-07-30 15:13:14 +0330

    fix edit permission bug and add test

--- backend/real_estate_offices/tests.py
+++ backend/real_estate_offices/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from common import codes
-from common.utils.test import login, test_invalid_field
+from common.utils.test import login, test_invalid_field, test_have_fields
 
 from realtors.models import Realtor
 
@@ -21,15 +21,16 @@ class CreateRealEstateOfficeTest(APITestCase):
                 'site': 'test.ir',
                 'linkedin': 'test',
                 'instagram':'test',
-                'telegram': 'test'
+                'telegram': 'test',
         }
 
         self.access, self.number = login(self, self.number)
         self.headers = {'Authorization': 'Bearer '+ self.access}
 
         # create Realtor
-        data = {}
-        self.client.post(reverse('create_realtor'), data, headers=self.headers)
+        data = {'description':'test is test', 'number':'09120120102', 'landline_number':'0211287821'}
+        resp = self.client.post(reverse('create_realtor'), data, headers=self.headers)
+        self.assertEqual(resp.data['status'], 200, resp.data)
         
         # confirm the realtor
         realtor = Realtor.objects.get(user__number=self.number)
@@ -54,7 +55,7 @@ class CreateRealEstateOfficeTest(APITestCase):
     def test_invlid_field(self):
         test_invalid_field(self, self.url, self.defult_data, "@/\\<>", self.headers)
 
-    def test_right(self):
+    def test_success(self):
 
         resp = self.client.post(self.url, self.default_data, headers=self.headers)
         self.assertEqual(resp.data['status'], 200, resp.data)
@@ -68,3 +69,19 @@ class CreateRealEstateOfficeTest(APITestCase):
         resp = self.client.post(self.url, data, headers=self.headers)
         self.assertEqual(resp.data['status'], 400, resp.data)
         self.assertEqual(resp.data['code'], codes.USER_IS_ALREADY_REO_OWNER, resp.data)
+
+
+class GetAllRealEstateOfficesTest(APITestCase):
+    def setUp(self):
+        self.url = reverse('get_all_real_estate_offices')
+        self.number = '09111111111'
+        self.access, self.number = login(self, self.number)
+        self.headers = {'Authorization': 'Bearer '+ self.access}
+
+
+
+    def test_success(self):
+        resp = self.client.get(self.url+'?page=1', headers=self.headers)
+        self.assertEqual(resp.data['status'], 200, resp.data)
+        self.assertNotEqual(resp.data.get('data', ''), '', resp.data)
+        test_have_fields(self, resp.data['data'][0], ['name', 'username', 'city', 'main_street', 'sub_street', 'score', 'number_of_actice_ads', 'number_of_comments', 'image_full_path', 'blue_tick'])        
--- backend/real_estate_offices/urls.py
+++ backend/real_estate_offices/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('create', views.CreateRealEstateOfficeAPIView.as_view(), name='create_real_estate_office'),
-    path('edit', views.EditRealEstateOfficeAPIView.as_view(), name='eidit_real_estate_office'),
+    path('edit/<int:real_estate_office_id>', views.EditRealEstateOfficeAPIView.as_view(), name='eidit_real_estate_office'),
     path('get-all', views.GetAllRealEstateOfficeAPIView.as_view(), name='get_all_real_estate_offices'),
     path('get/<slug>', views.GetRealEstateOfficeAPIView.as_view(), name='get_real_estate_office'),
     path('upload-image', views.UploadRealEstateOfficeImageAPIView.as_view(), name='upload_real_estate_office_image'),
