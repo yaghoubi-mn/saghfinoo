5259cd731a7a7aaa791c686488791c19e29c1885
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-08-10 01:41:21 +0330

    add comment for realtor

--- backend/realtors/models.py
+++ backend/realtors/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 from users.models import CustomUser
 from real_estate_offices.models import RealEstateOffice
+
+from common.utils.database import formated_datetime_now
 
 class Realtor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
@@ -9,7 +12,7 @@ class Realtor(models.Model):
     is_confirmed_by_real_estate_office = models.BooleanField(default=False) # must confirmed by real estate office owner
     is_confirmed = models.BooleanField(default=False) # must confirmed by admin
 
-    score = models.FloatField(default=5)
+    score = models.FloatField(default=settings.REALTOR_DEFAULT_SCORE)
     number_of_active_ads = models.PositiveIntegerField(default=0)
 
     bg_image = models.CharField(max_length=1000, default='') # background image
@@ -19,8 +22,22 @@ class Realtor(models.Model):
     number = models.CharField(max_length=11)
     landline_number = models.CharField(max_length=50)
 
+    score_num = models.PositiveIntegerField(default=0)
+    score_sum = models.PositiveBigIntegerField(default=0)
+
     def __str__(self):
         reo = ''
         if self.real_estate_office:
             reo = self.real_estate_office.name
         return self.user.first_name +' '+ self.user.last_name +', '+ reo
+    
+
+class Comment(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    score = models.FloatField()
+    description = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(default=formated_datetime_now)
+    modified_at = models.DateTimeField(default=formated_datetime_now)
