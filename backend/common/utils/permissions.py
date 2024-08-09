6c109d1a56d59ab96cc77d8f1823162aa7b68946
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-08-10 01:43:06 +0330

    fix error and add IsOwner

--- backend/common/utils/permissions.py
+++ backend/common/utils/permissions.py
@@ -1,7 +1,6 @@
 from rest_framework.permissions import BasePermission
 
-from . import realtor_model
-from real_estate_offices.models import RealEstateOffice
+from realtors.models import Realtor
 
 class IsRealEstateOfficeOwner(BasePermission):
 
@@ -22,13 +21,19 @@ class IsRealtor(BasePermission):
     
     def has_permission(self, request, view):
         try:
-            realtor = realtor_model.objects.get(user=request.user, is_confirmed=True)
+            realtor = Realtor.objects.get(user=request.user, is_confirmed=True)
             request.realtor = realtor
             return True
-        except realtor_model.DoesNotExist:
+        except Realtor.DoesNotExist:
             return False
 
 class IsAdvertisementOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner.user == request.user
+    
+
+class IsOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
