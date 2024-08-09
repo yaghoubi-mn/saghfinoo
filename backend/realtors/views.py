e0d7156b77221d23a71d11efd21a1b1785f3fdf3
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-08-09 17:54:39 +0330

    add total pages

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -98,8 +98,10 @@ class SearchRealtorsAPIView(APIView):
             query &= Q(real_estate_office__username=req.query_params['reo_username'])
 
         reo = Realtor.objects.values(*RealtorPreviewResponseSerializer.Meta.fields).filter(query)[page*limit: page*limit+limit]
+        total_pages = Realtor.objects.filter(query).count()/limit
+        
     
-        return Response({'data':reo, 'status':200})
+        return Response({'data':reo, 'total_pages':total_pages, 'status':200})
 
 
 
