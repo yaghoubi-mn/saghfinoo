a724d8b3c794ee6d2fd1daa70b6a3bef79a5cc53
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-08-27 20:29:56 +0330

    fix ScoreManger methods name

--- backend/common/utils/database.py
+++ backend/common/utils/database.py
@@ -9,7 +9,7 @@ def formated_datetime_now():
 
 class ScoreManager:
     @staticmethod
-    def increase_realtor_score(score, obj):
+    def increase_obj_score(score, obj):
         """obj must have score, score_sum and score_num"""
         obj.score_sum += score
         obj.score_num += 1
@@ -17,7 +17,7 @@ class ScoreManager:
         obj.save()
 
     @staticmethod
-    def edit_realtor_score(old_score, new_score, obj):
+    def edit_obj_score(old_score, new_score, obj):
         """obj must have score, score_sum and score_num"""
         obj.score_sum += new_score-old_score
         if obj.score_num == 0:
@@ -30,7 +30,7 @@ class ScoreManager:
         obj.save()
 
     @staticmethod
-    def decrease_realtor_score(score, obj):
+    def decrease_obj_score(score, obj):
         """on deleting comment
         obj must have score, score_sum and score_num"""
         obj.score_sum -= score
