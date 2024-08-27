ce6b0c90d2e34418930798b043d01cb29e038984
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-08-27 12:27:07 +0330

    add realtors comment score reason default values

--- backend/realtors/apps.py
+++ backend/realtors/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class RealtorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'realtors'
+
+    def ready(self):
+
+        try:
+            from .models import CommentScoreReason
+            CommentScoreReason.add_default_row()
+        except Exception as e:
+            print("EEROR in adding CommentScoreReason: "+str(e))
