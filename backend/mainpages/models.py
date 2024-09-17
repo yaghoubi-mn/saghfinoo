7202d30e1eebfc2d573efbf608f874a87df19141
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-09-17 18:46:37 +0330

    fix mainpages

--- backend/mainpages/models.py
+++ backend/mainpages/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Mainpage(models.Model):
     order = models.IntegerField()
     page = models.CharField(max_length=50)
-    section = models.CharField(max_length=50)
-    content = models.TextField(max_length=10000, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=10000, blank=True)
+    btn_text = models.CharField(max_length=50)
+    icon = models.CharField(max_length=1000)
